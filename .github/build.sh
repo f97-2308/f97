50e6a4c930dfb79027b82d95d08d6c9e7eb20c10
f97 <huynhduckhoan@gmail.com>
2022-01-12 09:53:22 +0000

    :bug: fix build.sh script

--- .github/build.sh
+++ .github/build.sh
@@ -3,25 +3,26 @@ git config --global user.email "huynhduckhoan@gmail.com"
 git config --global user.name "f97"
 mkdir ./temp 
 cd ./temp
-rm -rf survps gh-pages
-git clone https://f97:$1@github.com/f97/survps.git 
-git clone -b gh-pages  https://f97:$1@github.com/f97/survps.git gh-pages
+
+rm -rf f97 gh-pages
+git clone https://f97:$1@github.com/f97/f97.git 
+git clone -b gh-pages  https://f97:$1@github.com/f97/f97.git gh-pages
 rm -rf ./gh-pages/*
 
-cp ./survps/README.md ./gh-pages/README.md 
+cp ./f97/README.md ./gh-pages/README.md 
+cd gh-pages
 
 # HTML
 npx github-readme-to-html
 mv dist/index.html .
 rm -rf dist/
-md-to-pdf README.md
 # HTML
 
 # PDF
 npx md-to-pdf README.md
 mv README.pdf HUYNHDUCKHOAN_CV.pdf
 
-# PDF
-git add .
-git commit -m ':zap: update new CV'
-git push
+# # PDF
+# git add .
+# git commit -m ':zap: update new CV'
+# git push

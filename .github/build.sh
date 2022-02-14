5fdad719f8482dba03d52c6239dfb7fbcd4b4103
f97 <35532964+f97@users.noreply.github.com>
2022-02-14 08:41:29 +0000

    :boom: try with `replace-in-files-cli`

--- .github/build.sh
+++ .github/build.sh
@@ -15,13 +15,13 @@ cd gh-pages
 # HTML
 npx github-readme-to-html
 mv dist/index.html .
-sed -i 's/Read Me/HUYNH DUC KHOAN cv/' index.html
+npx replace-in-files-cli --string='Read Me' --replacement="HUYNH DUC KHOAN's cv" HUYNHDUCKHOAN_CV.md
 rm -rf dist/
 # HTML
 
 # PDF
 cp README.md HUYNHDUCKHOAN_CV.md
-sed -i 's/|PDF|[HUYNHDUCKHOAN_CV.pdf](https://cv.f97.xyz/HUYNHDUCKHOAN_CV.pdf)|||//' HUYNHDUCKHOAN_CV.md
+npx replace-in-files-cli --string='|PDF|[HUYNHDUCKHOAN_CV.pdf](https://cv.f97.xyz/HUYNHDUCKHOAN_CV.pdf)|||' --replacement='' HUYNHDUCKHOAN_CV.md
 npx md-to-pdf HUYNHDUCKHOAN_CV.md
 echo 'cv.f97.xyz'> CNAME
 rm HUYNHDUCKHOAN_CV.md
--- HUYNHDUCKHOAN_CV.md
+++ HUYNHDUCKHOAN_CV.md
@@ -5,7 +5,7 @@ Huỳnh Đức Khoản | HUYNH DUC KHOAN
 |---|---|---|---|
 |Email|huynhduckhoan@gmail.com|Blog|[https://f97.xyz](https://f97.xyz)|
 |Skype|[live:huynhduckhoan](https://join.skype.com/invite/niUom6aZDDwa)|Linkedin|[in/huynhduckhoan](https://www.linkedin.com/in/huynhduckhoan)|
-|PDF|[HUYNHDUCKHOAN_CV.pdf](https://cv.f97.xyz/HUYNHDUCKHOAN_CV.pdf)|||
+
 
 Technical Experience
 --------------------

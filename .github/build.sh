14fef8bd8b844b8e24c6633cdb13d0a996101ef4
f97 <35532964+f97@users.noreply.github.com>
2022-02-14 08:30:04 +0000

    :bug: update md-to-pdf

--- .github/build.sh
+++ .github/build.sh
@@ -20,12 +20,11 @@ rm -rf dist/
 # HTML
 
 # PDF
-cp README.md CV.md
-npx md-to-pdf README.md
-mv README.pdf HUYNHDUCKHOAN_CV.pdf
+cp README.md HUYNHDUCKHOAN_CV.md
+npx md-to-pdf HUYNHDUCKHOAN_CV.md
 echo 'cv.f97.xyz'> CNAME
 sed -i 's/|PDF|[HUYNHDUCKHOAN_CV.pdf](https://cv.f97.xyz/HUYNHDUCKHOAN_CV.pdf)|||//' index.html
-rm CV.md
+rm HUYNHDUCKHOAN_CV.md
 
 # GIT PUSH
 git add .
